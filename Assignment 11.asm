ed3e2b38c45738f13d6eb851ffc0a71acac9be29
poitrasandrew <poitrasandrew@gmail.com>
2018-05-14 15:44:53 -0700

    Update and rename Assignment 11 to Assignment 11.asm

---
+++ Assignment 11.asm
@@ -0,0 +1,28 @@
+;Author: Andrew Poitras
+;Assignment 11 Ch4.10 #2
+
+ExitProcess proto
+.data
+	array BYTE 10h, 20h, 30h, 40h, 50h, 60h, 70h, 80h
+	index BYTE 0
+.code
+main proc
+	mov esi, OFFSET array
+	mov edx,0
+	mov ecx,2
+	mov eax,LENGTHOF array
+	div ecx
+	mov ecx,eax
+	mov eax,TYPE array
+	mov ebx,2
+	mul ebx
+
+L1:
+	mov al,[esi]
+	xchg al,[esi + 1]
+	mov [esi],al
+	add esi,ebx
+	loop L1
+  call ExitProcess
+main endp
+end
